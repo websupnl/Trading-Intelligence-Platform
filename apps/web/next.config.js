edfa8483872ce1b4b9657c849d926ff71b38a87e
websupnl <info@websup.nl>
2026-05-27 11:42:40 +0200

    update

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api-proxy/:path*',
-        destination: `${process.env.INTERNAL_API_URL || 'http://api:8000'}/:path*`,
-      },
-    ];
-  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
