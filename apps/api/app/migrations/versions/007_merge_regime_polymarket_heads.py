29348abe9c4c22f1ccecc491f7f39a3e158a29ca
root <root@srv428916.hstgr.cloud>
2026-06-01 19:47:27 +0000

    fix: merge alembic regime and polymarket heads

---
+++ apps/api/app/migrations/versions/007_merge_regime_polymarket_heads.py
@@ -0,0 +1,22 @@
+"""Merge regime and polymarket migration heads
+
+Revision ID: 007_merge_regime_polymarket_heads
+Revises: 006_regime_states, 005_polymarket
+Create Date: 2026-06-01 19:45:00.000000
+
+"""
+from typing import Sequence, Union
+
+
+revision: str = "007_merge_regime_polymarket_heads"
+down_revision: Union[str, tuple[str, str], None] = ("006_regime_states", "005_polymarket")
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
