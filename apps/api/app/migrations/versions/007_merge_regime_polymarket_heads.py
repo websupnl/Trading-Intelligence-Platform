77b041cb2d62ac4270a5aabcb30f822c6371b27b
root <root@srv428916.hstgr.cloud>
2026-06-01 19:49:44 +0000

    fix: shorten alembic merge revision id

--- apps/api/app/migrations/versions/007_merge_regime_polymarket_heads.py
+++ apps/api/app/migrations/versions/007_merge_regime_polymarket_heads.py
@@ -1,6 +1,6 @@
 """Merge regime and polymarket migration heads
 
-Revision ID: 007_merge_regime_polymarket_heads
+Revision ID: 007_merge_heads
 Revises: 006_regime_states, 005_polymarket
 Create Date: 2026-06-01 19:45:00.000000
 
@@ -8,7 +8,7 @@ Create Date: 2026-06-01 19:45:00.000000
 from typing import Sequence, Union
 
 
-revision: str = "007_merge_regime_polymarket_heads"
+revision: str = "007_merge_heads"
 down_revision: Union[str, tuple[str, str], None] = ("006_regime_states", "005_polymarket")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
